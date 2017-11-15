ccf02cb0d9a3434beaf910b96a3d9f51677236c5
Leo Schweizer <leonhard.schweizer@gmail.com>
2017-11-15 21:54:07 +0100

    fixed indent

--- GDAX.lua
+++ GDAX.lua
@@ -83,18 +83,18 @@ end
 
 function base64decode(data)
 	local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-    data = string.gsub(data, '[^'..b..'=]', '')
-    return (data:gsub('.', function(x)
-        if (x == '=') then return '' end
-        local r,f='',(b:find(x)-1)
-        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
-        return r;
-    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
-        if (#x ~= 8) then return '' end
-        local c=0
-        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
-        return string.char(c)
-    end))
+	data = string.gsub(data, '[^'..b..'=]', '')
+	return (data:gsub('.', function(x)
+		if (x == '=') then return '' end
+		local r,f='',(b:find(x)-1)
+		for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+		return r;
+	end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+		if (#x ~= 8) then return '' end
+		local c=0
+		for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+		return string.char(c)
+	end))
 end
 
 function queryGdaxApi(endpoint)
@@ -106,7 +106,7 @@ function queryGdaxApi(endpoint)
 	headers["CB-ACCESS-KEY"] = apiKey
 	headers["CB-ACCESS-TIMESTAMP"] = timestamp
 	headers["CB-ACCESS-SIGN"] = MM.base64(apiSign)
-	headers["CB-ACCESS-PASSPHRASE"] = "2vhb5c82cbw"
+	headers["CB-ACCESS-PASSPHRASE"] = ""
 
 	local content = Connection():request("GET", url .. path, nil, nil, headers)
 	return JSON(content):dictionary()
